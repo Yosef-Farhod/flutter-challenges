b486e0611f0c52df921cc95ad36d739d7ce74a37
Yosef Farhod <xoup87@gmail.com>
2025-11-13 22:00:21 +0200

    Edit a b c

--- learn/t14_quadratic_solver.dart
+++ learn/t14_quadratic_solver.dart
@@ -1,6 +1,9 @@
 import 'dart:math';
 
 List<double> sq(double a, double b, double c) {
+  
+  List<double> x = [];
+
   double delta = (pow(b, 2) - (4 * b * c));
 
   if (delta > 0) {
@@ -12,7 +15,6 @@ List<double> sq(double a, double b, double c) {
     return [];
   }
 
-  List<double> x = [];
 
   x.add((-b - (sqrt(pow(b, 2) - (4 * a * c)))) / 2 * a);
   x.add((-b + (sqrt(pow(b, 2) - (4 * a * c)))) / 2 * a);
@@ -22,9 +24,9 @@ List<double> sq(double a, double b, double c) {
 
 void main() {
 
-  double a = 4;
-  double b = 4;
+  double a = 1;
+  double b = -3;
   double c = 2;
-  
+
   print(" x^2 - 3x + 2  = ${sq(a, b, c)}");
 }
