baabc6fac953fb2c84e95e68406d73d4b0b3c4b6
Yosef Farhod <xoup87@gmail.com>
2025-12-06 19:02:49 +0200

    Add class user

--- learn/t15.dart
+++ learn/t15.dart
@@ -1,14 +1,33 @@
 void main() {
   Book thesun = Book("thesun", "Ali");
   thesun.printTitle();
+
+
+  User ali = User(name: "Ali");
+  ali.show_data_user();
+
+  User Yosef = User(name: "Yosef", age: 22);
+  Yosef.show_data_user();
 }
 
 class Book {
   String title = "";
   String auther = "";
-  Book(this.title, this.auther) {}
+  Book(this.title, this.auther);
   void printTitle() {
     print("The title : $title");
   }
 }
 
+class User {
+  String name = "";
+  int? age;
+  User({required this.name, this.age}) {}
+
+  void show_data_user() {
+    print("The name is : $name");
+    if (age != null) {
+      print("Age : $age");
+    }
+  }
+}
